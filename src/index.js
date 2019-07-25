dc4c8ce5d0ddec7933edeaba8a3359151cf5cd5f
Renaud Genevois <renaud.genevois23@gmail.com>
2019-07-25 22:45:56 +0200

    suggestion liste

--- src/index.js
+++ src/index.js
@@ -1,25 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Editor from "./js/editor";
-import { getTokens } from "./js/parser-vtl";
-import * as serviceWorker from "./serviceWorker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Editor from './js/editor';
+import { getTokens } from './js/parser-vtl';
+import * as serviceWorker from './serviceWorker';
 
 // const content = ["if (a>0) ", "\tthen 'positive'", "\telse 'negative'"];
-const content = [
-  'var1   = "toto"',
-  "toto_2=7",
-  'last=var1+ "unfinished string',
-  "if(a=2)",
-  "\tthen 5",
-  "\telse 3"
-];
-const dico = { variables: ["toto", "var_1", "TATA", "TITI", "var_2", "titi"] };
+const content = [ 'var1   = "toto"', 'toto_2=7', 'last=var1+ "unfinished string', 'if(a=2)', '\tthen 5', '\telse 3' ];
+const dico = { variables: [ 'toto', 'var_1', 'TATA', 'TITI', 'var_2', 'titi', 'renaud', 'Lolo' ] };
 
 ReactDOM.render(
-  <React.Fragment>
-    <Editor content={content} dictionnary={dico} getTokens={getTokens} />
-  </React.Fragment>,
-  document.getElementById("root")
+	<React.Fragment>
+		<Editor content={content} dictionnary={dico} getTokens={getTokens} />
+	</React.Fragment>,
+	document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
--- src/js/editor/sugestions.component.js
+++ src/js/editor/sugestions.component.js
@@ -1,28 +1,42 @@
-import React, { useMemo, useState, useContext } from "react";
-import { EditorContext } from "./editor.component";
+import React, { useMemo, useState, useContext } from 'react';
+import { EditorContext } from './editor.component';
 
 const Suggestions = ({ suggest }) => {
-  const { cursorRect, prefix } = useContext(EditorContext);
+	const { cursorRect, prefix } = useContext(EditorContext);
 
-  const suggestions = useMemo(() => (prefix ? suggest(prefix) : {}), [
-    suggest,
-    prefix
-  ]);
-  return getState(suggestions) ? (
-    <div
-      className="suggestions"
-      style={{
-        left: `${Math.round(cursorRect.right)}px`,
-        top: `${Math.round(cursorRect.bottom)}px`
-      }}
-    />
-  ) : null;
+	const suggestions = useMemo(() => (prefix ? suggest(prefix) : {}), [ suggest, prefix ]);
+
+	return getState(suggestions) ? (
+		<div
+			className="suggestions"
+			style={{
+				left: `${Math.round(cursorRect.right)}px`,
+				top: `${Math.round(cursorRect.bottom)}px`
+			}}
+		>
+			{suggestions.variables.map((value, i) => <Item key={value} value={value} type="var" />)}
+		</div>
+	) : null;
 };
 
+/* */
 const getState = (suggestions = {}) =>
-  Object.values(suggestions).reduce(
-    (a, t) => (t && Array.isArray(t) && t.length > 0) || a,
-    false
-  );
+	Object.values(suggestions).reduce((a, t) => (t && Array.isArray(t) && t.length > 0) || a, false);
+
+/* */
+const Item = ({ value, type }) => {
+	return (
+		<div
+			className="suggestion"
+			onMouseDown={(e) => {
+				e.stopPropagation();
+				console.log(value);
+			}}
+		>
+			<span className="type">{type}</span>
+			<span className="valeur">{value}</span>
+		</div>
+	);
+};
 
 export default Suggestions;
