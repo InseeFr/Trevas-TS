73a6c33a424c114077e63841293b5f34ca79d856
Renaud Genevois <renaud.genevois23@gmail.com>
2019-07-27 08:45:28 +0200

    lorem ipsum

--- src/index.js
+++ src/index.js
@@ -3,14 +3,29 @@ import ReactDOM from 'react-dom';
 import Editor from './js/editor';
 import { getTokens } from './js/parser-vtl';
 import * as serviceWorker from './serviceWorker';
+import { LoremIpsum } from 'lorem-ipsum';
 
-// const content = ["if (a>0) ", "\tthen 'positive'", "\telse 'negative'"];
+const getWords = () => {
+	const lorem = new LoremIpsum({
+		sentencesPerParagraph: {
+			max: 8,
+			min: 4
+		},
+		wordsPerSentence: {
+			max: 160,
+			min: 50
+		}
+	});
+
+	return Object.keys(lorem.generateWords(100).split(' ').reduce((a, x) => ({ ...a, [x]: null }), {}));
+};
+
 const content = [ 'var1   = "toto"', 'toto_2=7', 'last=var1+ "unfinished string', 'if(a=2)', '\tthen 5', '\telse 3' ];
 const dico = { variables: [ 'toto', 'var_1', 'TATA', 'TITI', 'var_2', 'titi', 'renaud', 'Lolo' ] };
 
 ReactDOM.render(
 	<React.Fragment>
-		<Editor content={content} dictionnary={dico} getTokens={getTokens} />
+		<Editor content={content} dictionnary={{ variables: getWords() }} getTokens={getTokens} />
 	</React.Fragment>,
 	document.getElementById('root')
 );
--- src/js/editor/editor.component.js
+++ src/js/editor/editor.component.js
@@ -1,181 +1,168 @@
-import React, { useMemo, createContext, useEffect, useReducer } from "react";
-import PropTypes from "prop-types";
-import Line from "./line.component";
-import editorReducer, { initializer } from "./editor.reducer";
-import * as actions from "./editor.actions";
-import KEY from "./key-bind";
-import Suggestions from "./sugestions.component";
-import createSuggester from "./suggestions-manager";
+import React, { useMemo, createContext, useEffect, useReducer } from 'react';
+import PropTypes from 'prop-types';
+import Line from './line.component';
+import editorReducer, { initializer } from './editor.reducer';
+import * as actions from './editor.actions';
+import KEY from './key-bind';
+import Suggestions from './sugestions.component';
+import createSuggester from './suggestions-manager';
 
-import "./editor.scss";
+import './editor.scss';
 
 const Editor = ({ content = [], getTokens, dictionnary = {} }) => {
-  const [state, dispatch] = useReducer(editorReducer, getTokens, initializer);
-  const { lines, index, focusedRow } = state;
-  const tokensEl = [];
+	const [ state, dispatch ] = useReducer(editorReducer, getTokens, initializer);
+	const { lines, index, focusedRow } = state;
+	const tokensEl = [];
 
-  useEffect(() => {
-    dispatch({
-      type: "change-editor-content",
-      lines: content
-    });
-  }, [content, getTokens]);
+	useEffect(
+		() => {
+			dispatch({
+				type: 'change-editor-content',
+				lines: content
+			});
+		},
+		[ content, getTokens ]
+	);
 
-  const suggester = useMemo(() => createSuggester(dictionnary), [dictionnary]);
-  return (
-    <EditorContext.Provider value={{ ...state, dispatch, tokensEl }}>
-      <div
-        className="editor"
-        onKeyDown={keyDownCallback(dispatch, state, tokensEl)}
-        onMouseDown={onMouseDownCallback(dispatch, state)}
-        onBlur={onBlurCallback(dispatch, state)}
-      >
-        {lines.map(({ tokens, value }, i) => (
-          <Line
-            key={`${i}-line`}
-            tokens={tokens}
-            length={value.length}
-            number={i}
-            index={index}
-            focused={focusedRow === i}
-          />
-        ))}
-        <Suggestions suggest={suggester} />
-      </div>
-    </EditorContext.Provider>
-  );
+	const suggester = useMemo(() => createSuggester(dictionnary), [ dictionnary ]);
+	return (
+		<EditorContext.Provider value={{ ...state, dispatch, tokensEl }}>
+			<div
+				className="editor"
+				onKeyDown={keyDownCallback(dispatch, state, tokensEl)}
+				onMouseDown={onMouseDownCallback(dispatch, state)}
+				onBlur={onBlurCallback(dispatch, state)}
+			>
+				{lines.map(({ tokens, value }, i) => (
+					<Line
+						key={`${i}-line`}
+						tokens={tokens}
+						length={value.length}
+						number={i}
+						index={index}
+						focused={focusedRow === i}
+					/>
+				))}
+				<Suggestions suggest={suggester} />
+			</div>
+		</EditorContext.Provider>
+	);
 };
 
 /* */
-const onBlurCallback = dispatch => e => {
-  e.stopPropagation();
-  e.preventDefault();
-  dispatch(actions.exitEditor());
+const onBlurCallback = (dispatch) => (e) => {
+	e.stopPropagation();
+	e.preventDefault();
+	dispatch(actions.exitEditor());
 };
 
 /* */
-const onMouseDownCallback = (dispatch, state) => e => {
-  const { prefix } = state;
-  if (prefix) dispatch(actions.resetPrefix());
+const onMouseDownCallback = (dispatch, state) => (e) => {
+	const { prefix } = state;
+	if (prefix) dispatch(actions.resetPrefix());
 };
 
 /* */
-const keyDownCallback = (dispatch, state, tokensEl) => e => {
-  if (KEY.isUnbindedKey(e.key)) return;
-  e.stopPropagation();
-  e.preventDefault();
-  const { key } = e;
-  switch (key) {
-    case KEY.ARROW_UP:
-    case KEY.ARROW_DOWN:
-      dispatch({ type: key });
-      dispatch(actions.checkIndex());
-      dispatch(actions.resetPrefix());
-      break;
-    case KEY.DELETE:
-    case KEY.ENTER:
-    case KEY.BACK_SPACE:
-      if (isSelection()) {
-        checkForDeleteSelection(dispatch, tokensEl);
-        break;
-      }
-      dispatch({ type: key });
-      dispatch(actions.checkPrefix());
-      break;
-    case KEY.PAGE_UP:
-    case KEY.PAGE_DOWN:
-    case KEY.TAB:
-    case KEY.HOME:
-    case KEY.END:
-    case KEY.CONTEXT_MENU:
-    case KEY.ARROW_LEFT:
-    case KEY.ARROW_RIGHT:
-      dispatch({ type: key });
-      dispatch(actions.resetPrefix());
-      break;
-    default:
-      if (isCharCode(key)) {
-        dispatch(actions.insertCharacter(key));
-        dispatch(actions.checkPrefix());
-      }
-      break;
-  }
+const keyDownCallback = (dispatch, state, tokensEl) => (e) => {
+	if (KEY.isUnbindedKey(e.key)) return;
+	e.stopPropagation();
+	e.preventDefault();
+	const { key } = e;
+	switch (key) {
+		case KEY.ARROW_UP:
+		case KEY.ARROW_DOWN:
+			dispatch({ type: key });
+			dispatch(actions.checkIndex());
+			dispatch(actions.resetPrefix());
+			break;
+		case KEY.DELETE:
+		case KEY.ENTER:
+		case KEY.BACK_SPACE:
+			if (isSelection()) {
+				checkForDeleteSelection(dispatch, tokensEl);
+				break;
+			}
+			dispatch({ type: key });
+			dispatch(actions.checkPrefix());
+			break;
+		case KEY.PAGE_UP:
+		case KEY.PAGE_DOWN:
+		case KEY.TAB:
+		case KEY.HOME:
+		case KEY.END:
+		case KEY.CONTEXT_MENU:
+		case KEY.ARROW_LEFT:
+		case KEY.ARROW_RIGHT:
+			dispatch({ type: key });
+			dispatch(actions.resetPrefix());
+			break;
+		default:
+			if (isCharCode(key)) {
+				dispatch(actions.insertCharacter(key));
+				dispatch(actions.checkPrefix());
+			}
+			break;
+	}
 };
 
-const isCharCode = c => true; //c && /[\w!@#$%^&*(),.?":{}|<>].{1}/g.test(c);
+const isCharCode = (c) => true; //c && /[\w!@#$%^&*(),.?":{}|<>].{1}/g.test(c);
 
 /* */
 const isSelection = () => {
-  const {
-    anchorOffset,
-    focusOffset,
-    anchorNode,
-    focusNode
-  } = window.getSelection();
-  return !focusNode.isEqualNode(anchorNode) || anchorOffset !== focusOffset;
+	const { anchorOffset, focusOffset, anchorNode, focusNode } = window.getSelection();
+	return !focusNode.isEqualNode(anchorNode) || anchorOffset !== focusOffset;
 };
 
 const checkForDeleteSelection = (dispatch, tokensEl) => {
-  const selection = tokensEl.reduce(
-    (a, { spanEl, numberToken, numberRow, start, stop }) => {
-      return window
-        .getSelection()
-        .containsNode(spanEl.current.firstChild || spanEl.current)
-        ? numberRow in a
-          ? {
-              ...a,
-              [numberRow]: [
-                ...a[numberRow],
-                getNodeInformation({
-                  node: spanEl.current.firstChild,
-                  numberToken,
-                  start,
-                  stop
-                })
-              ]
-            }
-          : {
-              ...a,
-              [numberRow]: [
-                getNodeInformation({
-                  node: spanEl.current.firstChild,
-                  numberToken,
-                  start,
-                  stop
-                })
-              ]
-            }
-        : a;
-    },
-    {}
-  );
-  dispatch(actions.deleteSelction(selection));
-  const sel = window.getSelection();
-  sel.extend(sel.anchorNode, 0);
+	const selection = tokensEl.reduce((a, { spanEl, numberToken, numberRow, start, stop }) => {
+		return window.getSelection().containsNode(spanEl.current.firstChild || spanEl.current)
+			? numberRow in a
+				? {
+						...a,
+						[numberRow]: [
+							...a[numberRow],
+							getNodeInformation({
+								node: spanEl.current.firstChild,
+								numberToken,
+								start,
+								stop
+							})
+						]
+					}
+				: {
+						...a,
+						[numberRow]: [
+							getNodeInformation({
+								node: spanEl.current.firstChild,
+								numberToken,
+								start,
+								stop
+							})
+						]
+					}
+			: a;
+	}, {});
+	console.log(selection);
+	dispatch(actions.deleteSelction(selection));
+	const sel = window.getSelection();
+	sel.extend(sel.anchorNode, 0);
 };
 
 const getNodeInformation = ({ node, numberToken, start, stop }) => {
-  const {
-    anchorOffset,
-    focusOffset,
-    anchorNode,
-    focusNode
-  } = window.getSelection();
+	const { anchorOffset, focusOffset, anchorNode, focusNode } = window.getSelection();
 
-  if (anchorNode.isEqualNode(node))
-    return { numberToken, start: start + anchorOffset, stop };
-  else if (focusNode.isEqualNode(node))
-    return { numberToken, start, stop: start + focusOffset - 1 };
-  return { numberToken, start, stop };
+	if (anchorNode.isEqualNode(node)) return { numberToken, start: start + anchorOffset, stop };
+	else if (focusNode.isEqualNode(node)) return { numberToken, start, stop: start + focusOffset - 1 };
+	return { numberToken, start, stop };
 };
 
 /* */
 Editor.proTypes = {
-  getTokens: PropTypes.func.isRequired,
-  content: PropTypes.arrayOf(PropTypes.shape({ value: PropTypes.string })),
-  dictionnary: PropTypes.shape({
-    variables: PropTypes.arrayOf(PropTypes.string)
-  })
+	getTokens: PropTypes.func.isRequired,
+	content: PropTypes.arrayOf(PropTypes.shape({ value: PropTypes.string })),
+	dictionnary: PropTypes.shape({
+		variables: PropTypes.arrayOf(PropTypes.string)
+	})
 };
 
 export const TokenContext = createContext({});
