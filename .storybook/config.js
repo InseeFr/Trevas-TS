eeac144b95383c6e13a3ee8e2a20e581093c69d7
Nicolas Laval <nicolas.laval@insee.fr>
2019-08-30 11:03:06 +0200

    Update Storybook configuration

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import { configure, addDecorator } from '@storybook/react';
+import { addDecorator, addParameters, configure } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs/react';
 
 const globalContainer = { margin: '5%' };
 
 addDecorator(story => <div style={globalContainer}>{story()}</div>);
 addDecorator(withKnobs);
+
+addParameters({
+	options: {
+		name: 'VTL Tools',
+		addonPanelInRight: true,
+	},
+});
 
 const requireAll = requireContext => requireContext.keys().map(requireContext);
 const loadStories = () =>
