940ca5259946318e20197472a1f63d946770809c
Nicolas Laval <nicolas.laval@insee.fr>
2019-09-01 08:50:51 +0200

    Fix configuration

--- rollup.config.js
+++ rollup.config.js
@@ -22,9 +22,16 @@ export default {
 		builtins(),
 		resolve(),
 		postcss(),
-		babel(),
-		replace(),
-		commonjs(),
+		babel({
+			exclude: 'node_modules/**',
+		}),
+		replace({
+			exclude: 'node_modules/**',
+			ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
+		}),
+		commonjs({
+			exclude: 'src/**',
+		}),
 		autoExternal(),
 	],
 };
