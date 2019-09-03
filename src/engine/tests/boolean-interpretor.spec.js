0dae1bc79642925515c926c06b4fec9690e7678c
Franck Cotton <franck.cotton@insee.fr>
2019-09-03 11:40:13 +0200

    More test cases

--- src/engine/tests/boolean-interpretor.spec.js
+++ src/engine/tests/boolean-interpretor.spec.js
@@ -2,19 +2,29 @@ import interpret from '../interpretor';
 
 describe('interpretor', () => {
 	describe('interpret', () => {
-		it('should preserve numeric literal', () => {
-			expect(interpret('11', {})).toEqual(11);
+		it('true and true should be true', () => {
+			expect(interpret('true and true', {})).toEqual(true);
 		});
-		it('should preserve boolean literal', () => {
-			expect(interpret('true', {})).toEqual(true);
+		it('true and false should be false', () => {
+			expect(interpret('true and false', {})).toEqual(false);
 		});
-		it('should preserve string literal', () => {
-			// String between double quotes otherwise interpreted as variable
-			expect(interpret('"marvin"', {})).toEqual('marvin');
+		it('not (true) should be false', () => {
+			expect(interpret('not (true)', {})).toEqual(false);
 		});
-		it('should preserve empty string literal', () => {
-			// String between double quotes otherwise interpreted as variable
-			expect(interpret('""', {})).toEqual('');
+		it('not 1 should throw an error', () => {
+			expect(() => {
+				interpret('not 1', {})
+			}).toThrow();
+		});
+		it('true and 1 should throw an error', () => {
+			expect(() => {
+				interpret('true and 1', {})
+			}).toThrow();
+		});
+		it('3 or false should throw an error', () => {
+			expect(() => {
+				interpret('3 or false', {})
+			}).toThrow();
 		});
 	});
 });
