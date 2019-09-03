ea9138c249ae1ec4dd21a04cf5161eb07b3fec55
Nicolas Laval <nicolas.laval@insee.fr>
2019-09-03 14:11:53 +0200

    Update build-exec-object.js

--- src/components/engine/utils/build-exec-object.js
+++ src/components/engine/utils/build-exec-object.js
@@ -1,7 +1,13 @@
 export const buildExecObject = array =>
 	Array.isArray(array)
 		? array.reduce(
-				(_, { key, value }) => (key ? { ..._, [key]: parseFloat(value) } : _),
+				(_, { key, value }) => (key ? { ..._, [key]: cast(value) } : _),
 				{}
 		  )
 		: {};
+
+const cast = value => {
+	if (parseFloat(value)) return parseFloat(value);
+	else if (['true', 'false'].includes(value.trim())) return JSON.parse(value);
+	return value;
+};
