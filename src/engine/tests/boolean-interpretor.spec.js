473188f7c659e3e0a06c72948ff585cc022dfbde
Franck Cotton <franck.cotton@insee.fr>
2019-09-03 11:52:33 +0200

    More boolean tests

--- src/engine/tests/boolean-interpretor.spec.js
+++ src/engine/tests/boolean-interpretor.spec.js
@@ -2,13 +2,23 @@ import interpret from '../interpretor';
 
 describe('interpretor', () => {
 	describe('interpret', () => {
-		it('true and true should be true', () => {
+		it('should respect basic boolean logic', () => {
 			expect(interpret('true and true', {})).toEqual(true);
-		});
-		it('true and false should be false', () => {
 			expect(interpret('true and false', {})).toEqual(false);
+			expect(interpret('false and true', {})).toEqual(false);
+			expect(interpret('false and false', {})).toEqual(false);
+			expect(interpret('true or true', {})).toEqual(true);
+			expect(interpret('true or false', {})).toEqual(true);
+			expect(interpret('false or true', {})).toEqual(true);
+			expect(interpret('false or false', {})).toEqual(false);
+			expect(interpret('true xor true', {})).toEqual(false);
+			expect(interpret('true xor false', {})).toEqual(true);
+			expect(interpret('false xor true', {})).toEqual(true);
+			expect(interpret('false xor false', {})).toEqual(false);
+			expect(interpret('not true', {})).toEqual(false);
+			expect(interpret('not false', {})).toEqual(true);
 		});
-		it('not (true) should be false', () => {
+		it('not (true) with parenthesis should be false', () => {
 			expect(interpret('not (true)', {})).toEqual(false);
 		});
 		it('not 1 should throw an error', () => {
