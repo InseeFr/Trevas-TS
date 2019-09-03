8bb8ce7a7bc03758b89056d820ceec7f60781443
Hadrien Kohl <hadrien.kohl@gmail.com>
2019-09-03 11:24:38 +0200

    Add float and integer literal tests

--- src/engine/tests/literal-interpretor.spec.js
+++ src/engine/tests/literal-interpretor.spec.js
@@ -2,11 +2,25 @@ import interpret from '../interpretor';
 
 describe('interpretor', () => {
 	describe('interpret', () => {
-		it('should preserve numeric literal', () => {
-			expect(interpret('11', {})).toEqual(11);
+		it('should preserve integer literal', () => {
+			expect(interpret('12345', {})).toEqual(12345);
+			expect(interpret('-12345', {})).toEqual(-12345);
 		});
-		it('should preserve boolean literal', () => {
+		it('should preserve float literals', () => {
+			expect(interpret('1.', {})).toEqual(1.0);
+			expect(interpret('1.1', {})).toEqual(1.1);
+			expect(interpret('1.5e0', {})).toEqual(1.5);
+			expect(interpret('1.5e+0', {})).toEqual(1.5);
+			expect(interpret('1.5e-0', {})).toEqual(1.5);
+			expect(interpret('1.e0', {})).toEqual(1.0);
+			expect(interpret('1.5E1', {})).toEqual(15.0);
+			expect(interpret('1.5E-1', {})).toEqual(0.15);
+			expect(interpret('1.5E+1', {})).toEqual(15.0);
+			expect(interpret('1.E+1', {})).toEqual(10);
+		});
+		it('should preserve boolean literals', () => {
 			expect(interpret('true', {})).toEqual(true);
+			expect(interpret('false', {})).toEqual(true);
 		});
 		it('should preserve string literal', () => {
 			// String between double quotes otherwise interpreted as variable
