b63c14d01c44632ad37b326c128c56141261ebc8
Renaud Genevois <renaud.genevois@insee.fr>
2019-09-12 11:14:36 +0200

    refactor selection blocs

--- src/components/editor-component/components/tools/selection.js
+++ src/components/editor-component/components/tools/selection.js
@@ -5,68 +5,101 @@ const SELECTION = {};
 /*
  * SELECTION
  */
-const singleRowSelection = chasse => ({
+const singleRowSelection = () => ({
 	selection,
 	scrollRange,
 	rowHeight,
+	horizontalRange: hr,
+	chasse,
 }) => {
+	if (selection.start.index > hr.stop || selection.stop.index < hr.start)
+		return [];
 	const top = rowHeight * (selection.start.row - scrollRange.start);
-	const left = getCursorLeft(chasse)(selection.start.index);
-	const width = getCursorLeft(chasse)(selection.stop.index) - left;
+	const left = Math.max(
+		0,
+		getCursorLeft(chasse)(selection.start.index - hr.start)
+	);
+	const width =
+		Math.max(getCursorLeft(chasse)(selection.stop.index - hr.start), 0) - left;
 	return [{ top, width, left }];
 };
 
 /* */
-const multiRowSelection = chasse => ({
+const lastRow = row => ({ scrollRange, chasse, rowHeight, selection }) => {
+	const top = (row - scrollRange.start) * rowHeight;
+	const left = 0;
+	const width = selection.stop.index * chasse;
+	return {
+		top,
+		left,
+		width,
+	};
+};
+
+/* */
+const firstRow = row => ({
+	scrollRange,
+	chasse,
+	rowHeight,
 	selection,
 	lines,
-	scrollRange,
-	rowHeight,
 }) => {
+	const top = (row - scrollRange.start) * rowHeight;
+	const left = selection.start.index * chasse;
+	const width = (lines[row].value.length - selection.start.index) * chasse;
+	return {
+		top,
+		left,
+		width,
+	};
+};
+
+/* */
+const middleRow = row => ({ scrollRange, rowHeight, lines, chasse }) => {
+	const top = (row - scrollRange.start) * rowHeight;
+	const left = 0;
+	const width = lines[row].value.length * chasse;
+	return {
+		top,
+		left,
+		width,
+	};
+};
+
+/* */
+const getRow = row => state => {
+	const { selection } = state;
+	if (row === selection.start.row) {
+		return firstRow(row)(state);
+	}
+	if (row === selection.stop.row) {
+		return lastRow(row)(state);
+	}
+	return middleRow(row)(state);
+};
+
+/* */
+const multiRowSelection = () => state => {
+	const { selection, scrollRange } = state;
 	const blocs = new Array(selection.stop.row - selection.start.row + 1)
 		.fill({})
 		.reduce((a, b, i) => {
 			const scrStart = selection.start.row - scrollRange.start;
-			const scrStop = selection.stop.row - scrollRange.start;
-			const rowCurrent = scrStart + i;
-			const bloc =
-				rowCurrent >= 0 && rowCurrent <= scrollRange.offset - 1
-					? [
-							...a,
-							rowCurrent === scrStart
-								? {
-										top: rowCurrent * rowHeight,
-										left: selection.start.index * chasse,
-										width:
-											chasse *
-											(lines[selection.start.row + i].value.length -
-												selection.start.index),
-								  }
-								: rowCurrent === scrStop
-								? {
-										top: rowCurrent * rowHeight,
-										left: 0,
-										width: chasse * selection.stop.index,
-								  }
-								: {
-										top: rowCurrent * rowHeight,
-										left: 0,
-										width: chasse * lines[selection.start.row + i].value.length,
-								  },
-					  ]
-					: a;
+			const scrRow = scrStart + i;
+			const row = selection.start.row + i;
 
-			return bloc;
+			return scrRow >= 0 && scrRow <= scrollRange.offset - 1
+				? [...a, getRow(row)(state)]
+				: a;
 		}, []);
-
 	return blocs;
 };
 
 /* */
-export const getSelectionBlocs = chasse => state =>
+export const getSelectionBlocs = () => state =>
 	state.selection.start.row === state.selection.stop.row
-		? singleRowSelection(chasse)(state)
-		: multiRowSelection(chasse)(state);
+		? singleRowSelection()(state)
+		: multiRowSelection()(state);
 
 SELECTION.getSelectionBlocs = getSelectionBlocs;
 
