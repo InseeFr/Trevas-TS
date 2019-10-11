1c15c0d57b83930b0ab3a954798329d2273a78ee
Hadrien Kohl <hadrien.kohl@gmail.com>
2019-10-11 16:56:03 +0200

    Add regenerated files

--- src/antlr-tools/vtl-2.0-Insee/parser-vtl/index.js
+++ src/antlr-tools/vtl-2.0-Insee/parser-vtl/index.js
@@ -2,4 +2,11 @@ export { default as VtlLexer } from './VtlLexer';
 export { default as VtlParser } from './VtlParser';
 export { default as VtlListener } from './VtlListener';
 export { default as VtlVisitor } from './VtlVisitor';
-export { default as TreeVtlVisitor } from './TreeVtlVisitor';
+
+// TODO: this fails ATM, @nico?
+// const VtlParser = require('./VtlParser');
+// const VtlLexer = require('./VtlLexer');
+// const VtlVisitor = require('./VtlVisitor');
+// const VtlListener = require('./VtlListener');
+//
+// export default { VtlParser, VtlLexer, VtlVisitor, VtlListener }
