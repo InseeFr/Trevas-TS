75dda286dc8bd3852ab137f12c7f893237a62d8e
Nicolas Laval <nicolas.laval@insee.fr>
2019-10-14 10:37:03 +0200

    Fix antlr tools interface

--- src/antlr-tools/vtl-2.0-Insee/parser-vtl/index.js
+++ src/antlr-tools/vtl-2.0-Insee/parser-vtl/index.js
@@ -1,12 +1,4 @@
-export { default as VtlLexer } from './VtlLexer';
-export { default as VtlParser } from './VtlParser';
-export { default as VtlListener } from './VtlListener';
-export { default as VtlVisitor } from './VtlVisitor';
-
-// TODO: this fails ATM, @nico?
-// const VtlParser = require('./VtlParser');
-// const VtlLexer = require('./VtlLexer');
-// const VtlVisitor = require('./VtlVisitor');
-// const VtlListener = require('./VtlListener');
-//
-// export default { VtlParser, VtlLexer, VtlVisitor, VtlListener }
+export { VtlLexer } from './VtlLexer';
+export { VtlParser } from './VtlParser';
+export { VtlListener } from './VtlListener';
+export { VtlVisitor } from './VtlVisitor';
