e3504575b40288f6073cac1014033e739735aeeb
Nicolas Laval <nicolas.laval@insee.fr>
2020-01-09 15:26:22 +0100

    Update expression visitor

--- src/engine/visitors/Expression.js
+++ src/engine/visitors/Expression.js
@@ -17,25 +17,33 @@ class ExpressionVisitor extends VtlVisitor {
 		super();
 		this.bindings = bindings;
 		this.arithmeticVisitor = new ArithmeticVisitor(this);
+		this.booleanAlgebraVisitor = new BooleanAlgebraVisitor(this);
+		this.castFunctionVisitor = new CastVisitor(this);
+		this.comparisonVisitor = new ComparisonVisitor(this);
+		this.concatenationVisitor = new ConcatenationVisitor(this);
+		this.ifThenElseVisitor = new IfThenElse(this);
+		this.literalVisitor = new LiteralVisitor();
+		this.numericFunctionVisitor = new NumericFunctionsVisitor(this);
+		this.stringFunctionVisitor = new StringFunctionsVisitor(this);
+		this.variableVisitor = new VariableVisitor(this.bindings);
 	}
 
-	visitComparisonExpr = ctx => new ComparisonVisitor(this).visit(ctx);
+	visitComparisonExpr = ctx => this.comparisonVisitor.visit(ctx);
 
 	visitArithmeticExpr = ctx => this.arithmeticVisitor.visit(ctx);
 	visitArithmeticExprOrConcat = ctx => this.arithmeticVisitor.visit(ctx);
 
 	visitUnaryExpr = ctx => {
 		const { op } = ctx;
-		if (op.type === VtlParser.NOT)
-			return new BooleanAlgebraVisitor(this).visit(ctx);
-		else return new ArithmeticVisitor(this).visit(ctx);
+		if (op.type === VtlParser.NOT) return this.booleanAlgebraVisitor.visit(ctx);
+		else return this.arithmeticVisitor.visit(ctx);
 	};
 
-	visitBooleanExpr = ctx => new BooleanAlgebraVisitor(this).visit(ctx);
+	visitBooleanExpr = ctx => this.booleanAlgebraVisitor.visit(ctx);
 
 	visitParenthesisExpr = ctx => this.visit(ctx.expr());
 
-	visitIfExpr = ctx => new IfThenElse(this).visit(ctx);
+	visitIfExpr = ctx => this.ifThenElseVisitor.visit(ctx);
 
 	// TODO: Optional expression should handle missing values.
 	visitOptionalExpr = ctx =>
@@ -43,17 +51,17 @@ class ExpressionVisitor extends VtlVisitor {
 	visitFunctionsExpression = ctx => this.visit(ctx.functions());
 	visitGenericFunctions = ctx => this.visit(ctx.genericOperators());
 
-	visitVarIdExpr = ctx => new VariableVisitor(this.bindings).visit(ctx);
+	visitVarIdExpr = ctx => this.variableVisitor.visit(ctx);
 
-	visitConstantExpr = ctx => new LiteralVisitor().visit(ctx);
+	visitConstantExpr = ctx => this.literalVisitor.visit(ctx);
 
 	// Functions
-	visitCastExprDataset = ctx => new CastVisitor(this).visit(ctx);
-	visitConcatExpr = ctx => new ConcatenationVisitor(this).visit(ctx);
+	visitCastExprDataset = ctx => this.castFunctionVisitor.visit(ctx);
+	visitConcatExpr = ctx => this.concatenationVisitor.visit(ctx);
 	visitStringFunctions = ctx =>
-		new StringFunctionsVisitor(this).visit(ctx.stringOperators());
+		this.stringFunctionVisitor.visit(ctx.stringOperators());
 	visitNumericFunctions = ctx =>
-		new NumericFunctionsVisitor(this).visit(ctx.numericOperators());
+		this.numericFunctionVisitor.visit(ctx.numericOperators());
 }
 
 export default ExpressionVisitor;
