2eeb1176b1849b02e49b94dce6f81cad96712807
Hadrien Kohl <hadrien.kohl@gmail.com>
2020-01-09 15:00:26 +0100

    Add comparison tests with floats

--- src/engine/tests/interpretors/comparison-interpretor.spec.js
+++ src/engine/tests/interpretors/comparison-interpretor.spec.js
@@ -19,5 +19,22 @@ describe('interpretor', () => {
 			expect(interpret('1 = 2', {})).toEqual(false);
 			expect(interpret('2 <> 1', {})).toEqual(true);
 		});
+		it('should compare floats', () => {
+			expect(interpret('1.1 < 1.11', {})).toEqual(true);
+			expect(interpret('1.1 <= 1.1', {})).toEqual(true);
+			expect(interpret('1.11 <= 1.1', {})).toEqual(false);
+			expect(interpret('1.11 < 1.1', {})).toEqual(false);
+
+			expect(interpret('1.1 > 1.11', {})).toEqual(false);
+			expect(interpret('1.1 >= 1.1', {})).toEqual(true);
+			expect(interpret('1.11 >= 1.1', {})).toEqual(true);
+			expect(interpret('1.11 > 1.1', {})).toEqual(true);
+
+			expect(interpret('1.1 = 1.1', {})).toEqual(true);
+			expect(interpret('1.1 <> 1.1', {})).toEqual(false);
+
+			expect(interpret('1.1 = 1.2', {})).toEqual(false);
+			expect(interpret('1.1 <> 1.2', {})).toEqual(true);
+		});
 	});
 });
