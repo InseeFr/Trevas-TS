066782f47912b76a33f31c593cd6bf6a24a2ff52
Franck Cotton <franck.cotton@insee.fr>
2020-01-10 11:38:25 +0100

    Completed unary numeric operators

--- src/engine/visitors/functions/NumericFunction.js
+++ src/engine/visitors/functions/NumericFunction.js
@@ -7,6 +7,7 @@ class NumericVisitor extends VtlVisitor {
 	}
 
 	visitUnaryNumeric = ctx => {
+		// Unary numeric operators are CEIL, FLOOR, ABS, EXP, LN and SQRT
 		const { op: opCtx } = ctx;
 
 		const expr = this.exprVisitor.visit(ctx.expr());
@@ -20,9 +21,29 @@ class NumericVisitor extends VtlVisitor {
 		let type;
 
 		switch (opCtx.type) {
+			case VtlParser.CEIL:
+				operatorFunction = expr => Math.ceil(expr);
+				type = VtlParser.INTEGER;
+				break;
+			case VtlParser.FLOOR:
+				operatorFunction = expr => Math.floor(expr);
+				type = VtlParser.INTEGER;
+				break;
 			case VtlParser.ABS:
 				operatorFunction = expr => Math.abs(expr);
 				type = expr.type;
+				break;
+			case VtlParser.EXP:
+				operatorFunction = expr => Math.exp(expr);
+				type = VtlParser.NUMBER;
+				break;
+			case VtlParser.LN:
+				operatorFunction = expr => Math.log(expr);
+				type = VtlParser.NUMBER;
+				break;
+			case VtlParser.SQRT:
+				operatorFunction = expr => Math.sqrt(expr);
+				type = VtlParser.NUMBER;
 				break;
 			default:
 				throw new Error(`unknown operator ${opCtx.getText()}`);
