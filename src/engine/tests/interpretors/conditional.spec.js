cec0206941d2d487c130027e8fdd03e254a87d6e
Hadrien Kohl <hadrien.kohl@gmail.com>
2020-01-15 14:39:36 +0100

    Add tests for if-then-else expressions

--- src/engine/tests/interpretors/conditional.spec.js
+++ src/engine/tests/interpretors/conditional.spec.js
@@ -1,7 +1,15 @@
 import interpret from '../../interpretor';
+import { IncompatibleTypeError } from '../../errors';
 
 describe('interpretor', () => {
 	describe('conditional operators', () => {
+		it('supports if-then-else', () => {
+			expect(interpret('if (true) then "true" else "false"')).toEqual('true');
+			expect(interpret('if (false) then "true" else "false"')).toEqual('false');
+			expect(() => interpret('if (false) then "notnumber" else 42')).toThrow(
+				IncompatibleTypeError
+			);
+		});
 		it('tests on the nvl function', () => {
 			expect(interpret('nvl(5, 0)', {})).toEqual(5);
 			expect(interpret('nvl(null, 0)', {})).toEqual(0);
