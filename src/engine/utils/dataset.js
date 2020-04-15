d7ae5ed863e27b43e97eba2a5a24bdaae4f8c9c7
romaintailhurat <romain.tailhurat@gmail.com>
2020-04-15 16:14:50 +0200

    Smallest refacto on dataset transform

--- src/engine/utils/dataset.js
+++ src/engine/utils/dataset.js
@@ -4,17 +4,17 @@ export const fromDatasetToDataframe = dataset => {
 	const columnNames = Object.keys(dataset.dataStructure);
 	const dpColumns = Object.keys(dataset.dataPoints);
 	const l = dpColumns.length;
-	let dfColumns = [];
+	let rows = [];
 	for (let i = 0; i < l; i++) {
 		let a = [];
 		for (const col of dpColumns) {
 			a.push(dataset.dataPoints[col][i]);
 		}
-		dfColumns.push(a);
+		rows.push(a);
 	}
 
 	const df = new DataFrame({
-		rows: dfColumns,
+		rows: rows,
 		columnNames: columnNames,
 	});
 	return df;
