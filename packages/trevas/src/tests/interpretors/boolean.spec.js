65789d882507c3079acf74644ea173fc2c351d97
NicoLaval <nicolas.laval@insee.fr>
2021-05-28 21:33:47 +0200

    Support null into Boolean visitor

--- packages/trevas/src/tests/interpretors/boolean.spec.js
+++ packages/trevas/src/tests/interpretors/boolean.spec.js
@@ -1,7 +1,22 @@
 import interpret from '../../interpretor';
+import { transpose } from '../../utils';
 
 describe('interpretor', () => {
 	describe('interpret', () => {
+		describe('supports basic boolean logic with null', () => {
+			const A = [false, false, false, true, true, true, null, null, null];
+			const B = [false, true, null, false, true, null, false, true, null];
+			const AND = [false, false, false, false, true, null, false, null, null];
+			const OR = [false, true, null, true, true, true, null, true, null];
+			const XOR = [false, true, null, true, false, null, null, null, null];
+			test.each(transpose([A, B, AND, OR, XOR]))('', (a, b, and, or, xor) => {
+				expect(interpret(`${a} and ${b}`, {})).toEqual(and);
+				expect(interpret(`${a} or ${b}`, {})).toEqual(or);
+				expect(interpret(`${a} xor ${b}`, {})).toEqual(xor);
+			});
+			expect(interpret('not null', {})).toBeNull();
+			expect(interpret('not (null)', {})).toBeNull();
+		});
 		it('should respect basic boolean logic', () => {
 			expect(interpret('true and true', {})).toEqual(true);
 			expect(interpret('true and false', {})).toEqual(false);
@@ -35,10 +50,6 @@ describe('interpretor', () => {
 			expect(() => {
 				interpret('3 or false', {});
 			}).toThrow();
-		});
-		it('should preserve null literal', () => {
-			// String between double quotes otherwise interpreted as variable
-			expect(interpret('null', {})).toEqual(null);
 		});
 	});
 });
--- packages/trevas/src/utils/index.js
+++ packages/trevas/src/utils/index.js
@@ -4,3 +4,4 @@ export { getDate, getStringFromDate, getCurrentDate } from './dates';
 export { getTokenName } from './parser';
 export * from './datasets';
 export * from './null';
+export * from './array';
