8b4e1b440e5148054cac7ec2a56329e8d15ecb6c
NicoLaval <nicolas.laval@insee.fr>
2021-05-29 07:12:07 +0200

    Support null into Variable visitor

--- packages/trevas/src/tests/interpretors/variables.spec.js
+++ packages/trevas/src/tests/interpretors/variables.spec.js
@@ -16,11 +16,13 @@ describe('variable', () => {
 			aFloat: 12.34,
 			aDataset: { dataStructure: {}, dataPoints: {} },
 			aFlawedDataset: { dataPoints: {} },
+			nullValue: null,
 		};
 		expect(interpret('aString', bindings)).toEqual('string!');
 		expect(interpret('anInt', bindings)).toEqual(1234);
 		expect(interpret('aFloat', bindings)).toEqual(12.34);
 		expect(interpret('aDataset', bindings).toArray()).toEqual([]);
+		expect(interpret('nullValue', bindings)).toBeNull();
 		// FIXME how to bubble up the error thrown to the `interpret` function ?
 		//expect(interpret('aFlawedDataset', bindings)).toThrow();
 	});
