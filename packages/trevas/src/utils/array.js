ed18868adf93f26b3b42c49763213635341ced91
NicoLaval <nicolas.laval@insee.fr>
2021-07-12 13:48:22 +0200

    Remove inappropriate parseInt

--- packages/trevas/src/utils/array.js
+++ packages/trevas/src/utils/array.js
@@ -5,23 +5,17 @@ export const transpose = (array) =>
 
 export const getArraySum = (array) => {
 	if (!Array.isArray(array) || array.includes(null)) return null;
-	return array.reduce((acc, a) => acc + parseInt(a, 10), 0);
+	return array.reduce((acc, a) => acc + a, 0);
 };
 
 export const getArrayMin = (array) => {
 	if (!Array.isArray(array) || array.includes(null)) return null;
-	return array.reduce((acc, a) => {
-		const current = parseInt(a, 10);
-		return acc < current ? acc : current;
-	}, array[0]);
+	return array.reduce((acc, a) => (acc < a ? acc : a), array[0]);
 };
 
 export const getArrayMax = (array) => {
 	if (!Array.isArray(array) || array.includes(null)) return null;
-	return array.reduce((acc, a) => {
-		const current = parseInt(a, 10);
-		return acc > current ? acc : current;
-	}, array[0]);
+	return array.reduce((acc, a) => (acc > a ? acc : a), array[0]);
 };
 
 export const getArrayMedian = (array) => {
