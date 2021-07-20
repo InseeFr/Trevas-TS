a45b898a53f73a866ee256a02943447b8df3db41
NicoLaval <nicolas.laval@insee.fr>
2021-07-20 17:48:34 +0200

    Remove duplicated code

--- packages/trevas/src/utils/array.js
+++ packages/trevas/src/utils/array.js
@@ -1,50 +1,57 @@
-export const getArraySum = (array) => {
+const handleNull = (array) => (fn) => {
 	if (!Array.isArray(array) || array.includes(null)) return null;
-	return array.reduce((acc, a) => acc + a, 0);
+	return fn(array);
+};
+
+export const getArraySum = (array) => {
+	const fn = (arr) => arr.reduce((acc, a) => acc + a, 0);
+	return handleNull(array)(fn);
 };
 
 export const getArrayMin = (array) => {
-	if (!Array.isArray(array) || array.includes(null)) return null;
-	return array.reduce((acc, a) => (acc < a ? acc : a), array[0]);
+	const fn = (arr) => arr.reduce((acc, a) => (acc < a ? acc : a), array[0]);
+	return handleNull(array)(fn);
 };
 
-//handle null
 export const getArrayMax = (array) => {
-	if (!Array.isArray(array) || array.includes(null)) return null;
-	return array.reduce((acc, a) => (acc > a ? acc : a), array[0]);
+	const fn = (arr) => arr.reduce((acc, a) => (acc > a ? acc : a), array[0]);
+	return handleNull(array)(fn);
 };
 
 export const getArrayMedian = (array) => {
-	if (!Array.isArray(array) || array.includes(null)) return null;
-	const mid = Math.floor(array.length / 2);
-	const sortedArray = [...array].sort((a, b) => a - b);
-	return array.length % 2 !== 0
-		? sortedArray[mid]
-		: (sortedArray[mid - 1] + sortedArray[mid]) / 2;
+	const fn = (arr) => {
+		const mid = Math.floor(arr.length / 2);
+		const sortedArray = [...arr].sort((a, b) => a - b);
+		return arr.length % 2 !== 0
+			? sortedArray[mid]
+			: (sortedArray[mid - 1] + sortedArray[mid]) / 2;
+	};
+	return handleNull(array)(fn);
 };
 
 export const getArrayAvg = (array) => {
-	if (!Array.isArray(array) || array.includes(null)) return null;
-	return array.reduce((acc, a) => acc + a, 0) / array.length;
+	const fn = (arr) => arr.reduce((acc, a) => acc + a, 0) / array.length;
+	return handleNull(array)(fn);
 };
 
 // usePopulation = true: population deviation
 // usePopulation = false: sample deviation
 export const getDeviation = (array, usePopulation = true) => {
-	if (!Array.isArray(array) || array.includes(null)) return null;
-	if (array.length <= 1) return 0;
-	const mean = array.reduce((acc, val) => acc + val, 0) / array.length;
-	return Math.sqrt(
-		array
-			.reduce((acc, val) => acc.concat((val - mean) ** 2), [])
-			.reduce((acc, val) => acc + val, 0) /
-			(array.length - (usePopulation ? 0 : 1))
-	);
+	const fn = (arr) => {
+		if (arr.length <= 1) return 0;
+		const mean = arr.reduce((acc, val) => acc + val, 0) / arr.length;
+		return Math.sqrt(
+			arr
+				.reduce((acc, val) => acc.concat((val - mean) ** 2), [])
+				.reduce((acc, val) => acc + val, 0) /
+				(arr.length - (usePopulation ? 0 : 1))
+		);
+	};
+	return handleNull(array)(fn);
 };
 
 export const getVariance = (array, usePopulation) => {
 	const deviation = getDeviation(array, usePopulation);
-	if (deviation === 0) return 0;
 	if (deviation === null) return null;
 	return deviation ** 2;
 };
