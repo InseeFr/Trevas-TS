f595848f23b22fc2abbd43f865a13e0608dfc6a2
Hadrien Kohl <hadrien.kohl@gmail.com>
2024-07-20 08:33:28 +0200

    Improve getTokenType

--- src/utils/context.ts
+++ src/utils/context.ts
@@ -1,3 +1,7 @@
 import { ParserRuleContext } from "@making-sense/antlr4ng";
+import { TypeMismatchError } from "errors";
 
-export const getTokenType = (ctx: ParserRuleContext): number | undefined => ctx?.start?.type;
+export const getTokenType = (ctx: ParserRuleContext): number => {
+    if (!ctx.start) throw new TypeMismatchError(ctx, 0, 0);
+    return ctx.start.type;
+};
