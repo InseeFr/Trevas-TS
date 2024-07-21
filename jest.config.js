96ca51ce82114e121626062aa60397ccff9c261c
Hadrien Kohl <hadrien.kohl@gmail.com>
2024-07-21 08:55:14 +0200

    Delete jest.config.js

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    collectCoverageFrom: ["src/**/*.{js,jsx}", "!src/**/index.js", "!src/**/*.stories.js"],
-    transform: {
-        "\\.[jt]sx?$": "babel-jest"
-    },
-    coverageProvider: "v8",
-    testResultsProcessor: "jest-sonar-reporter",
-    coveragePathIgnorePatterns: ["/node_modules/"]
-};
