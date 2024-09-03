b96cbc29121f2c80256052e11d9cfab26249f467
Hadrien Kohl <hadrien.kohl@gmail.com>
2024-09-03 10:50:16 +0200

    Fix configuration

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
 export { default as interpret } from "./interpretor";
+
+export type { BasicScalarTypes, Bindings, Component, Dataset } from "model";
--- src/model/bindings.ts
+++ src/model/bindings.ts
@@ -1,7 +1,6 @@
 import * as dfd from "danfojs/dist/danfojs-browser/src";
 import { BasicScalarTypes, Component, Dataset } from "./vtl";
 
-// To define
 export type InternalDataset = { dataStructure: Component[]; dataset: dfd.DataFrame };
 
 export type Bindings = Record<string, BasicScalarTypes | Dataset>;
